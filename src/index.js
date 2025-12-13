e7ad5556126e997ba85d12fe15137b10a05773bc
Dylan Wickwire <dlwicky@hotmail.com>
2025-12-13 17:09:11 -0600

    added albums and started making my own json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 import express from "express";
 
 const app = express();
+
 const port = 3000;
+
+app.use(express.static('public'))
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
@@ -13,4 +17,22 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
+});
+
+const albums= [
+    {Artist: "Reflections", Title: "The Color Clear", Genre:"Djent"},
+    {Artist: "The Devil Wears Prada", Title: "With Roots Above and Branches Below", Genre:"Metalcore"},
+    {Artist: "Woe Is Me", Title: "Numbers", Genre:"Metalcore"},
+    {Artist: "Attack Attack!", Title: "Self Titled", Genre:"Metalcore"},
+    {Artist: "Thy Art Is Murder", Title: "Hate", Genre:"Deathcore"},
+]
+
+app.get('/albums', (req, res) => {
+  res.json(albums);
+});
+
+app.post('/albums', (req, res) => {
+  // Route handlers can modify the items array
+  items.push(req.body);
+  res.status(201).json(req.body);
 });
