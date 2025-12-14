40f5ee45b30cead95b1dee9a1f6d9b572b60baa2
Dylan Wickwire <dlwicky@hotmail.com>
2025-12-13 20:23:45 -0600

    got the albums to post

--- src/index.js
+++ src/index.js
@@ -1,62 +1,42 @@
-// src/index.js
 import express from "express";
 
 const app = express();
-
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(express.json());
 
+const albums = [
+    { Artist: "Reflections", Title: "The Color Clear", Genre: "Djent" },
+    {
+        Artist: "The Devil Wears Prada",
+        Title: "With Roots Above and Branches Below",
+        Genre: "Metalcore",
+    },
+    { Artist: "Woe Is Me", Title: "Numbers", Genre: "Metalcore" },
+    { Artist: "Attack Attack!", Title: "Self Titled", Genre: "Metalcore" },
+    { Artist: "Thy Art Is Murder", Title: "Hate", Genre: "Deathcore" },
+];
 
 app.get("/", (req, res) => {
-   res.send('<h1>Hello Express!</h1><p>Your server is working!</p>');
+    res.send("<h1>Hello Express!</h1><p>Your server is working!</p>");
+});
+
+app.get("/albums", (req, res) => {
+    res.json({ total: albums.length, albums });
+});
+
+app.post("/albums", (req, res) => {
+  const newAlbum = {
+    Artist: req.body.Artist,
+    Title: req.body.Title,
+    Genre: req.body.Genre,
+  };
+
+  albums.push(newAlbum);
+  res.status(201).json(newAlbum);
 });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-});
-
-const albums= [
-    {Artist: "Reflections", Title: "The Color Clear", Genre:"Djent"},
-    {Artist: "The Devil Wears Prada", Title: "With Roots Above and Branches Below", Genre:"Metalcore"},
-    {Artist: "Woe Is Me", Title: "Numbers", Genre:"Metalcore"},
-    {Artist: "Attack Attack!", Title: "Self Titled", Genre:"Metalcore"},
-    {Artist: "Thy Art Is Murder", Title: "Hate", Genre:"Deathcore"},
-]
-
-app.get('/albums', (req, res) => {
-   
-  res.json(albums);
-});
-
-app.post('/albums', (req, res)=>{
-    // console.log(req.body)
-    
-    //create new album from user input
-    const newAlbum={
-        artist: req.artist,
-        title: req.title,
-        genre: req.title
-
-    }
-
-    //add new game to our list
-    albums.push(newAlbum)
-
-    //inform user of success 
-    res.status(201);
-    res.json(newAlbum);
-        
-    
-})
-
-app.post('/albums', (req, res) => {
-  // Route handlers can modify the items array
-  items.push(req.body);
-  res.status(201).json(req.body);
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
 });
